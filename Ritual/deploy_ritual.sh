1c4a9bd8c041d9bc23d599b034576c21b74bfe9c
你的名字 <>
2025-07-12 10:21:21 +0800

    优化 Ritual 脚本：调整 batch_size 和 starting_sub_id 解决 missing trie node 错误

--- Ritual/deploy_ritual.sh
+++ Ritual/deploy_ritual.sh
@@ -519,8 +519,8 @@ cat <<EOF > "$HOME/infernet-container-starter/deploy/config.json"
     },
     "snapshot_sync": {
       "sleep": 3,
-      "batch_size": 500,
-      "starting_sub_id": 240000,
+      "batch_size": 100,
+      "starting_sub_id": 262002,
       "sync_period": 30
     }
   },
